1df28184aede1daca0becdeea30986bc54fe391d
Maksim Samuilionak <maximwow70@gmail.com>
2022-02-07 13:07:41 +0300

    feat: add local mongo db uri

--- backend/app/service/db-access.service.ts
+++ backend/app/service/db-access.service.ts
@@ -1,32 +1,33 @@
-import { Injectable } from '../ioc';
-import { Db, MongoClient } from 'mongodb';
+import { Injectable } from "../ioc";
+import { Db, MongoClient } from "mongodb";
 
 @Injectable()
 export class DbAccessService {
-	private mongoClient: MongoClient;
+  private mongoClient: MongoClient;
 
-	public get client(): MongoClient {
-		return this.mongoClient;
-	}
+  //   Choose one and use it in constructor
+  //   private static MONGO_URL_SERVER: string = `mongodb+srv://user:h08FMi9ubLAakYpw@cluster0.qrfka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+  private static MONGO_URL_LOCAL: string = `mongodb://localhost:27017`;
 
-	public get db(): Promise<Db> {
-		if (this.mongoClient.isConnected()) {
-			return Promise.resolve(this.mongoClient.db('gandalf-db'));
-		} else {
-			return this.mongoClient.connect().then((client: MongoClient) => {
-				this.mongoClient = client;
-				return client.db('gandalf-db');
-			});
-		}
-	}
+  public get client(): MongoClient {
+    return this.mongoClient;
+  }
 
-	constructor() {
-		this.mongoClient = new MongoClient(
-			`mongodb+srv://user:h08FMi9ubLAakYpw@cluster0.qrfka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			}
-		);
-	}
+  public get db(): Promise<Db> {
+    if (this.mongoClient.isConnected()) {
+      return Promise.resolve(this.mongoClient.db("gandalf-db"));
+    } else {
+      return this.mongoClient.connect().then((client: MongoClient) => {
+        this.mongoClient = client;
+        return client.db("gandalf-db");
+      });
+    }
+  }
+
+  constructor() {
+    this.mongoClient = new MongoClient(DbAccessService.MONGO_URL_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
 }

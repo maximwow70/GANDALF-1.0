bf350d6efc0222a5b112619d80b4117a0b0d57e9
egorwow70 <egor.samuilenok@gmail.com>
2022-02-16 16:39:29 +0300

    feat: extend task-card.component

--- frontend/src/app/pages/tasks-page/components/task-card/task-card.component.ts
+++ frontend/src/app/pages/tasks-page/components/task-card/task-card.component.ts
@@ -7,16 +7,20 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent {
-	@Input() task!: UserTask;
+	@Input() task: UserTask | null = null;
 	@Input() deleteDisabled!: boolean | null;
+	@Input() reviewMode: boolean = false;
+	@Input() highlighted: boolean = false;
 
 	@Output() openClicked: EventEmitter<void>;
 	@Output() editClicked: EventEmitter<void>;
+	@Output() reviewClicked: EventEmitter<void>;
 	@Output() deleteClicked: EventEmitter<void>;
 
 	constructor() {
 		this.openClicked = new EventEmitter<void>();
 		this.editClicked = new EventEmitter<void>();
+		this.reviewClicked = new EventEmitter<void>();
 		this.deleteClicked = new EventEmitter<void>();
 	}
 }
---
+++ frontend/src/app/pages/tasks-page/components/task-card/task-card.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskCardComponent } from './task-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+	declarations: [TaskCardComponent],
+	imports: [
+        CommonModule,
+        MatCardModule,
+        MatButtonModule,
+    ],
+	exports: [TaskCardComponent],
+})
+export class TaskCardModule {}

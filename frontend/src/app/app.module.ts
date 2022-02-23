bf7b695d7ce79b77fa77c374d345379c286bfa4c
Maksim Samuilionak <maximwow70@gmail.com>
2022-02-23 20:32:52 +0300

    refactor: code review

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContainerModule } from './container/container.module';
-import { HttpClientModule } from '@angular/common/http';
-
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from 'src/environments/environment';
 
 @NgModule({
--- frontend/src/app/container/container.module.ts
+++ frontend/src/app/container/container.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ContainerComponent } from './container.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { CommonComponentsModule } from '../common-components/common-components.module';
-import { AuthModule } from './auth/auth.module';
 import { CommonModule } from '@angular/common';
+
+import { CommonComponentsModule } from '../common-components/common-components.module';
+import { ContainerComponent } from './container.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
 	declarations: [ContainerComponent],
